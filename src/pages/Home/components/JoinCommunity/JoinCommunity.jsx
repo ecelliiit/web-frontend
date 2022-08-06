bb91ab637cf9c1970a2f8204c90a085748328c21
Prashant Kumar Singh <pk3076889@gmail.com>
2022-08-06 21:50:09 +0530

    toaster added

--- src/pages/Home/components/JoinCommunity/JoinCommunity.jsx
+++ src/pages/Home/components/JoinCommunity/JoinCommunity.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./JoinCommunity.scss";
 import { useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import CalendarSVG from "../../../../assets/illustrations/calendar.svg";
 import CommunitySVG from "../../../../assets/illustrations/community.svg";
@@ -9,8 +11,19 @@ import CommunitySVG from "../../../../assets/illustrations/community.svg";
 const JoinCommunity = () => {
   const [input, setInput] = useState(false);
 
+  const notify = () =>
+    toast("Subscribed Successfully", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+    });
+
   const handleInput = () => {
-    console.log(input);
+
     axios
       .post("http://604c-2405-201-6005-e12e-98d9-490-ebbd-3eaf.ngrok.io/subscriber", {
         first_name: "New",
@@ -22,7 +35,7 @@ const JoinCommunity = () => {
       })
       .catch((err) => {
         console.log(err);
-      });
+      }); 
   }
 
   return (
@@ -46,7 +59,7 @@ const JoinCommunity = () => {
                 setInput(e.target.value);
               }}
             />
-            <button onClick={handleInput}>Subscribe</button>
+            <button onClick={handleInput() ? notify() : console.log("error")}>Subscribe</button>
           </div>
         </div>
       </div>
