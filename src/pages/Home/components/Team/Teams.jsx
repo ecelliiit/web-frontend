259a66e7e68458ed9b231e7965f9ddbe5a559d02
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-02-02 01:28:38 +0530

    fixed error, removed css files with scss

--- src/pages/Home/components/Team/Teams.jsx
+++ src/pages/Home/components/Team/Teams.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Teams.scss";
-import "./Teams.css";
 
 const Teams = () => {
 
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -3,7 +3,7 @@ const data = [
     key: 1,
     name: "Aditya Rawat",
     position: "President",
-    img: "https://media-exp1.licdn.com/dms/image/C4D03AQHjAq-HVr-oPA/profile-displayphoto-shrink_400_400/0/1651491486062?e=1665014400&v=beta&t=uEBusUCGVjQ9r2qIEQHrk4p3DjR0Dxax5aNvIzCRS34"
+    img: "https://ik.imagekit.io/ecelliiitsonepat/WhatsApp_Image_2022-08-05_at_4.40.51_PM_6KcHjn_Qh.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1660306532824"
   },
   {
     key: 2,
@@ -15,7 +15,7 @@ const data = [
     key: 3,
     name: "Prateek Yaduvanshi",
     position: "Social Media Lead",
-    img: "https://media-exp1.licdn.com/dms/image/C4D03AQEFIP0UBMpA_A/profile-displayphoto-shrink_400_400/0/1641880243216?e=1665014400&v=beta&t=Rzc2lY6_Av2uHypPptINgoQRgDdtF930E2BphW7k0d4"
+    img: "https://ik.imagekit.io/ecelliiitsonepat/1641880243216__1_.jpeg_8u7L6mEFsc.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675275292840"
   },
   {
     key: 4,
@@ -39,7 +39,7 @@ const data = [
     key: 7,
     name: "Rishi Bachhuka",
     position: "Sponsorship Lead",
-    img: "https://media-exp1.licdn.com/dms/image/C5603AQFW5RmpgKYQVA/profile-displayphoto-shrink_400_400/0/1631274451893?e=1665014400&v=beta&t=g-1Aecc_F4GeCWesx-2stRCeEgk9dG1LMd0-2PtUGKA"
+    img: "https://ik.imagekit.io/ecelliiitsonepat/WhatsApp_Image_2022-08-11_at_9.23.13_PM_mHzgFoiG4.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1660306532583"
   },
 ];
 
